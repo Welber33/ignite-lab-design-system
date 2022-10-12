f0917a0d15098c37a9b2c2b79b2a33ba9778eddb
Welber33 <jwwork21@gmail.com>
2022-10-11 23:10:09 -0300

    Ignite Lab - Aula 02 (Do Figma ao React)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
+      <button className="bg-cyan-500 hover:bg-cyan-300 px-4 py-2 mt-3 text-center rounded-lg">
+        Enviar
+      </button>
+    </>
+    
+  )
+}
+
